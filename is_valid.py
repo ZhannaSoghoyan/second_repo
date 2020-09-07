87942ed8c5a720a9e525edf9a1129a75dd81ed31
Jeanne <jannasoghoyan1@gmail.com>
2020-09-07 17:58:51 +0400

    first commit in check_full_url branch, changed is_valid.py

--- is_valid.py
+++ is_valid.py
@@ -1,7 +1,7 @@
 from re import match
 string = input("enter url  ")
 
-is_valid = match(r'^http(s){0,1}:',string)
+is_valid = match(r'^http(s){0,1}[:]{1}[/]{2}[w]{3}[\.]{1}[\w]{1,}[\.]{1}[a-z]{3}',string)
 
 print("is valid url ",bool(is_valid))
 
